2bdb3e80858fa03f64906789c29602d4ca2da988
jungle0127 <arbor@foxmail.com>
2017-11-04 21:22:01 +0800

    added a demo for i++

---
+++ src/com/javastudy/test.java
@@ -0,0 +1,12 @@
+package com.javastudy;
+
+public class test {
+
+	public static void main(String[] args) {
+		// temp = j; j = j+1;j = temp;
+		int j = 0;
+		j = j++;
+		System.out.println(j);
+	}
+
+}
