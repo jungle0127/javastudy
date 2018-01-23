5af9f06a06da597d16dd9c9b376ccf2634624fee
jungle0127 <arbor@foxmail.com>
2018-01-23 22:52:14 +0800

    added tracing log for myservice

--- multithreads/src/main/java/org/multithreads/reentranlock/alternate/MyService.java
+++ multithreads/src/main/java/org/multithreads/reentranlock/alternate/MyService.java
@@ -1,5 +1,6 @@
 package org.multithreads.reentranlock.alternate;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -11,35 +12,45 @@ public class MyService {
 	public void set() {
 		try {
 			lock.lock();
+			TimeUnit.SECONDS.sleep(1);
+			System.out.println("a");
 			while(hasValue) {
 				condition.await();
+				System.out.println("b");
 			}
 			System.out.println("Print ***");
 			hasValue = true;
 			condition.signal();
+			System.out.println("c");
 		}
 		catch( InterruptedException e) {
 			e.printStackTrace();
 		}
 		finally {
 			lock.unlock();
+			System.out.println("d");
 		}
 	}
 	public void get() {
 		try {
 			this.lock.lock();
+			TimeUnit.SECONDS.sleep(1);
+			System.out.println("1");
 			while(!this.hasValue) {
 				this.condition.await();
+				System.out.println("2");
 			}
 			System.out.println("Print @@@");
 			this.hasValue = false;
 			this.condition.signal();
+			System.out.println("3");
 		}
 		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		finally {
 			this.lock.unlock();
+			System.out.println("4");
 		}
 	}
 }
