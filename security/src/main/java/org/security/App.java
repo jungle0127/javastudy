27b56a0d815e8ce4ddb8a5469f602d1258073bf8
jungle0127 <arbor@foxmail.com>
2018-01-31 17:19:31 +0800

    added some test code

--- security/src/main/java/org/security/App.java
+++ security/src/main/java/org/security/App.java
@@ -1,5 +1,12 @@
 package org.security;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+
 /**
  * Hello world!
  *
@@ -9,5 +16,18 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        try {
+        	Cipher ci;
+			MessageDigest md5 = MessageDigest.getInstance("SHA-512");
+			byte[] pwd = md5.digest("jinghe".getBytes("utf-8"));
+			BigInteger bi = new BigInteger(1, pwd);
+			System.out.println(bi.toString(16));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
