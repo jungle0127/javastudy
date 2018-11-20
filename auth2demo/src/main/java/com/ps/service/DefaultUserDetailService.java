f7637607425388389e37b29c1307c395944d7cb3
jungle0127 <arbor@foxmail.com>
2018-11-20 23:18:57 +0800

    add new demo project

---
+++ auth2demo/src/main/java/com/ps/service/DefaultUserDetailService.java
@@ -0,0 +1,21 @@
+package com.ps.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultUserDetailService implements UserDetailsService {
+    @Autowired
+    public PasswordEncoder passwordEncoder;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String password = this.passwordEncoder.encode("lotus");
+        return new User(username,password, AuthorityUtils.commaSeparatedStringToAuthorityList("adimin,ROLE_USER"));
+    }
+}
