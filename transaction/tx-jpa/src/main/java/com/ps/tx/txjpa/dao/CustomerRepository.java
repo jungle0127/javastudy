94e1b6fc7d768fa53d98a783f4e9a0546c0920a2
jungle0127 <arbor@foxmail.com>
2018-12-06 23:50:22 +0800

    move the transaction project out

--- transaction/tx-jpa/src/main/java/com/ps/tx/txjpa/dao/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ps.tx.txjpa.dao;
-
-import com.ps.tx.txjpa.domain.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Customer findOneByUsername(String username);
-
-}
